22cddcdc2a0c38789d1b24c07242953d61dfa953
MustaMohamed <mustafa.mohamed.wd@gmail.com>
2026-04-30 23:49:57 +0300

    feat: create database entity interfaces for Account and AppSetting

---
+++ database/entities/account.entity.ts
@@ -0,0 +1,21 @@
+import { AccountType, Currency } from '@/constants/enums';
+
+export interface Account {
+  id: string;
+  name: string;
+  type: AccountType;
+  currency: Currency;
+  opening_balance: number;
+  current_balance: number;
+  color: string | null;
+  credit_limit: number | null;
+  revolving_balance: number | null;
+  minimum_payment: number | null;
+  statement_due_day: number | null;
+  interest_tracking: 0 | 1;
+  apr: number | null;
+  is_archived: 0 | 1;
+  sort_order: number;
+  created_at: string;
+  updated_at: string;
+}
