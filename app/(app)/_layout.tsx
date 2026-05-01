db5fa55fb3f0ed3eb0aa6bf127749fbdcfbea037
MustaMohamed <mustafa.mohamed.wd@gmail.com>
2026-05-01 22:18:00 +0300

    feat(m2a): categories strings + wire loadCategories in app layout

--- app/(app)/_layout.tsx
+++ app/(app)/_layout.tsx
@@ -2,15 +2,18 @@ import { useEffect } from 'react';
 import { Stack } from 'expo-router';
 
 import { useAccountStore } from '@/store/account.store';
+import { useCategoryStore } from '@/store/category.store';
 import { useCurrencyStore } from '@/store/currency.store';
 
 export default function AppLayout() {
   const loadAccounts = useAccountStore((s) => s.loadAccounts);
+  const loadCategories = useCategoryStore((s) => s.loadCategories);
   const loadRate = useCurrencyStore((s) => s.loadRate);
   const fetchRate = useCurrencyStore((s) => s.fetchRate);
 
   useEffect(() => {
     loadAccounts().catch(() => {});
+    loadCategories().catch(() => {});
     loadRate()
       .then(() => fetchRate())
       .catch(() => {});
