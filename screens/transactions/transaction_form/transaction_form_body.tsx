269291873ce0951ceb33f674fd41ee10534d18a3
MustaMohamed <mustafa.mohamed.wd@gmail.com>
2026-05-04 23:43:30 +0300

    refactor(transaction_form_body): replace useState with state file

--- screens/transactions/transaction_form/transaction_form_body.tsx
+++ screens/transactions/transaction_form/transaction_form_body.tsx
@@ -1,6 +1,5 @@
 import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 import DateTimePicker, { DateTimePickerAndroid } from '@react-native-community/datetimepicker';
-import { useState } from 'react';
 import {
   KeyboardAvoidingView,
   Platform,
@@ -20,6 +19,7 @@ import { formatTime12h } from '@/utils/format_time_12h';
 import { ExchangeRateRow } from './components/exchange_rate_row';
 import { Numpad } from './components/numpad';
 import { TypeTabs } from './components/type_tabs';
+import { useTransactionFormBodyState } from './transaction_form_body.state';
 import type { Account } from '@/database/entities/account.entity';
 import type { Category } from '@/database/entities/category.entity';
 
@@ -98,8 +98,10 @@ export function TransactionFormBody({
   onClose,
   handleSave,
 }: TransactionFormBodyProps) {
-  const [showIosDatePicker, setShowIosDatePicker] = useState(false);
-  const [showIosTimePicker, setShowIosTimePicker] = useState(false);
+  const showIosDatePicker = useTransactionFormBodyState((s) => s.state.showIosDatePicker);
+  const setShowIosDatePicker = useTransactionFormBodyState((s) => s.setShowIosDatePicker);
+  const showIosTimePicker = useTransactionFormBodyState((s) => s.state.showIosTimePicker);
+  const setShowIosTimePicker = useTransactionFormBodyState((s) => s.setShowIosTimePicker);
   const isTransferOrCC = type === TransactionType.Transfer || type === TransactionType.CCPayment;
 
   const dateAsDate = new Date(date + 'T' + time);
@@ -122,7 +124,7 @@ export function TransactionFormBody({
         },
       });
     } else {
-      setShowIosDatePicker((v) => !v);
+      setShowIosDatePicker(!showIosDatePicker);
     }
   }
 
@@ -142,7 +144,7 @@ export function TransactionFormBody({
         },
       });
     } else {
-      setShowIosTimePicker((v) => !v);
+      setShowIosTimePicker(!showIosTimePicker);
     }
   }
 
