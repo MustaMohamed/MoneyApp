fc929c730e526ffd31223418f8df7f1a8acc88f8
MustaMohamed <mustafa.mohamed.wd@gmail.com>
2026-05-12 01:13:30 +0300

    feat: replace withNativeWind with withUniwindConfig + wrapWithReanimatedMetroConfig

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,10 @@
 const { getDefaultConfig } = require('expo/metro-config');
-const { withNativeWind } = require('nativewind/metro');
+const { withUniwindConfig } = require('uniwind/metro');
+const { wrapWithReanimatedMetroConfig } = require('react-native-reanimated/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-module.exports = withNativeWind(config, { input: './global.css' });
+module.exports = withUniwindConfig(wrapWithReanimatedMetroConfig(config), {
+  cssEntryFile: './global.css',
+  dtsFile: './uniwind.d.ts',
+});
