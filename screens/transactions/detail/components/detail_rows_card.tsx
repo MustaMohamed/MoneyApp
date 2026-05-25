497bfe6101d430749dc8f9ed69022e59ae2d1318
MustaMohamed <mustafa.mohamed.wd@gmail.com>
2026-05-25 15:45:17 +0300

    refactor(transactions): DetailRowsCard uses HeroUI Card substrate

--- screens/transactions/detail/components/detail_rows_card.tsx
+++ screens/transactions/detail/components/detail_rows_card.tsx
@@ -1,14 +1,10 @@
+import { Card } from 'heroui-native';
 import React from 'react';
-import { View } from 'react-native';
 
 interface Props {
   children: React.ReactNode;
 }
 
 export function DetailRowsCard({ children }: Props): React.ReactElement {
-  return (
-    <View className="bg-surface border-separator mx-4 mt-4 overflow-hidden rounded-2xl border">
-      {children}
-    </View>
-  );
+  return <Card className="mx-4 mt-4 overflow-hidden">{children}</Card>;
 }
